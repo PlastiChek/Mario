5ecdb824335d49738afffb22ecc8336ba34f5636
PlastiChek <mikhaylov010909@gmail.com>
2025-01-10 18:55:06 +0300

    second

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-from PIL import Image
 
 pygame.init()
 image = pygame.image.load('data/fon.jpg')
@@ -16,7 +15,44 @@ while running:
 
     screen.fill((0, 0, 0))
     screen.blit(new_image, (0, 0))
+    load_level('map.txt')
 
     pygame.display.flip()
 
 pygame.quit()
+
+
+def load_level(filename):
+    filename = "data/" + filename
+
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    max_width = max(map(len, level_map))
+
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+tile_images = {
+    'wall': load_image('box.png'),
+    'empty': load_image('grass.png')
+}
+player_image = load_image('mario.png')
+
+tile_width = tile_height = 50
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
